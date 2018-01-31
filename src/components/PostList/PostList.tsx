73e811eb3d4000a59d58d5b15f0bf385bd8194ba
dogrod <rodrickjue@hotmail.com>
2018-01-31 14:56:32 +0800

    :hammer: Basic component construction

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,6 +1,28 @@
 import * as React from 'react'
+import axios, { AxiosResponse } from 'axios'
+import { IResponse } from '../../../types/api';
 
 class PostList extends React.Component {
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      list: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const response: AxiosResponse<IResponse.PostList> = await axios.get('//api.dev.dogrod.xyz/blog/posts')
+  
+      this.setState({
+        list: response.data.results
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
     return (
       <div>Post List</div>
