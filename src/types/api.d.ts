aaf7e4b2c2e61e790ce2cf4db48440e8034110d9
dogrod <rodrickjue@hotmail.com>
2018-02-01 16:40:27 +0800

    :hammer: Move types file folder to /src

---
+++ src/types/api.d.ts
@@ -0,0 +1,17 @@
+import IBlog from './blog'
+
+declare namespace IResponse {
+  interface BasicList {
+    results: any[]
+    total: number
+    pageNumber: number
+    pageSize: number
+    pageCount: number
+  }
+
+  interface PostList extends BasicList {
+    results: IBlog.Post[]
+  }
+
+  const PostDetail: IBlog.Post
+}
---
+++ src/types/blog.d.ts
@@ -0,0 +1,30 @@
+export namespace IBlog {
+  interface Author {
+    email: string
+    username: string
+  }
+
+  interface User extends Author {
+    id: number
+    active: boolean
+    staff: boolean
+  }
+
+  interface Tag {
+    // id: number
+    name: number
+    slug: string
+  }
+
+  interface Post {
+    id: number
+    title: string
+    slug: string | string[]
+    publish_at: string
+    
+    author: Author
+    tags: Tag[]
+  }
+}
+
+export default IBlog
