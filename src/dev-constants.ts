dfa4458d158d8f33ff63ceebc4151fc8a248586f
dogrod <rodrickjue@hotmail.com>
2018-02-27 18:30:42 +0800

    :hammer: Preparing first depolyment

--- src/dev-constants.ts
+++ src/dev-constants.ts
@@ -2,5 +2,5 @@ export default {
   local: 'http://192.168.142.204:5901',
   dev: 'http://api.dev.dogrod.xyz',
   test: 'http://api.test.dogrod.xyz',
-  prod: 'http://api.dogrod.com',
+  production: 'http://api.dogrod.com',
 }
--- src/services/http/http.ts
+++ src/services/http/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import constants from '../../dev-constants'
 
-const env = process.env.SERVER_ENV || 'local'
+const env = process.env.NODE_ENV || 'local'
 const baseURL = constants[env]
 
 const httpRequest = axios.create({
