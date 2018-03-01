c2bdb1c0862a0a16381c9d0c924bb37a086489d2
dogrod <rodrickjue@hotmail.com>
2018-03-01 17:23:11 +0800

    :hammer: Specific development environment variable

--- src/dev-constants.ts
+++ src/dev-constants.ts
@@ -1,6 +1,6 @@
 export default {
   local: 'http://192.168.142.204:5901',
-  dev: 'http://api.dev.dogrod.xyz',
+  development: 'http://api.dev.dogrod.xyz',
   test: 'http://api.test.dogrod.xyz',
   production: 'http://api.dogrod.com',
 }
