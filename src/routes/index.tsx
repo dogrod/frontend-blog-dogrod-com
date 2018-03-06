53be72e578d3ec92379a6752a0a54ee61199d584
dogrod <rodrickjue@hotmail.com>
2018-03-06 18:00:48 +0800

    :hammer: Rename /views to /pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import PostList from 'views/PostList/index'
-import PostDetail from 'views/PostDetail/index'
+import PostList from 'pages/PostList/index'
+import PostDetail from 'pages/PostDetail/index'
 
 // const About = () => (
 //   <div>
