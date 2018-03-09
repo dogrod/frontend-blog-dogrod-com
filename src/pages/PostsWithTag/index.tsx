13e6a9c4cd05295d4bbfe78e9416443365f25b11
dogrod <rodrickjue@hotmail.com>
2018-03-09 17:47:19 +0800

    :sparkles: Add fetch data method

--- src/pages/PostsWithTag/index.tsx
+++ src/pages/PostsWithTag/index.tsx
@@ -1,9 +1,59 @@
 import * as React from 'react'
+import { RouteComponentProps } from 'react-router'
+import { IBlog } from 'types/blog';
+import { AxiosResponse } from 'axios'
 
-export default class PostsWithTag extends React.Component {
+import $http from 'services/http/http'
+import { IResponse } from 'types/api';
+
+interface RouteProps {
+  slug: string
+}
+
+interface Props extends RouteComponentProps<RouteProps> {}
+
+interface State {
+  slug: string
+  posts?: IBlog.Post[]
+  isLoading: boolean
+}
+
+export default class PostsWithTag extends React.Component<Props> {
+  state: State
+
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      slug: props.match.params.slug,
+      isLoading: true
+    }
+  }
+
+  async componentDidMount() {
+    await this.fetchPosts()
+
+    this.setState({
+      isLoading: false
+    })
+  }
+
   render() {
     return (
       <div>Posts with tag</div>
     )
+  }
+
+  private async fetchPosts() {
+    try {
+      const response: AxiosResponse<IResponse.PostList>
+        = await $http.get(`/blog/tags/${this.state.slug}`)
+
+      return this.setState({
+        posts: response.data.results
+      })
+    } catch (error) {
+      return console.error(error)
+    }
   }
 }
