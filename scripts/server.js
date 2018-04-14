eafbd5879e8142ac97d0068527c2054d4af22f32
dogrod <rodrickjue@hotmail.com>
2018-04-14 22:24:19 +0800

    :sparkles: Simple proxy for backend api

--- scripts/server.js
+++ scripts/server.js
@@ -1,25 +1,37 @@
 const Koa = require('koa')
 // const Router = require('koa-router')
-const Proxy = require('koa-better-http-proxy')
+const proxy = require('koa-better-http-proxy')
 
 const serve = require('koa-static')
 const send = require('koa-send')
 const path = require('path')
 
+const serverConfig = require(path.join(process.cwd(), 'server.config.js'))
+
 const app = new Koa()
 // const router = new Router()
 
-const port = parseInt(process.env.PORT) || 9002
+const port = parseInt(serverConfig.port, 10) || 9002
 
 app.use(serve(path.join(process.cwd(), 'build')))
 
 // app.use(router.routes())
 
 app.use(async (ctx, next) => {
-  await send(path.join(process.cwd(), 'build', 'index.html'))
+  // 如果请求头中含有json请求，则简单判定为非前端页面请求，直接跳过
+  if (
+    ctx.header.accept
+    && ctx.header.accept.indexOf('application/json' > -1)
+  ) {
+    return await next()
+  }
+
+  await send(ctx, path.join(process.cwd(), 'build', 'index.html'))
 
   await next()
 })
+
+app.use(proxy(serverConfig.proxy['/api']))
 
 app.listen(port, err => {
   if (err) throw err
