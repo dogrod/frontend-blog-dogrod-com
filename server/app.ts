11e2b0ae8fbcdcb9df632826b4553785d5f9d140
dogrod <rodrickjue@hotmail.com>
2018-04-23 13:11:55 +0800

    :fire: Remove koa-send

--- server/app.ts
+++ server/app.ts
@@ -3,7 +3,6 @@ import * as Koa from 'koa'
 import * as proxy from 'koa-better-http-proxy'
 
 import * as serve from 'koa-static'
-// import * as send from 'koa-send'
 import * as koaLogger from 'koa-logger'
 import * as path from 'path'
 
@@ -23,20 +22,6 @@ app.use(koaLogger())
 app.use(transmitBundleFile)
 
 app.use(serve(path.join(process.cwd(), 'build')))
-
-// router.get('*', async (ctx: Koa.Context, next: () => void) => {
-//   // 如果请求头中含有json请求，则简单判定为非前端页面请求，直接跳过
-//   if (
-//     ctx.header.accept
-//     && ctx.header.accept.indexOf('application/json') > -1
-//   ) {
-//     return await next()
-//   }
-
-//   const htmlPath = path.join(process.cwd(), 'build', 'index.html')
-//   log.info(`Output html path: ${htmlPath}`)
-//   await send(ctx, htmlPath)
-// })
 
 // app.use(router.routes())
 
