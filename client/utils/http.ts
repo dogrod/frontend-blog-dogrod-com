4ae2f731c88c15fcaab3d889cebb10c1d14c26c6
dogrod <rodrickjue@hotmail.com>
2018-05-28 17:15:39 +0800

    :sparkles: Client side http request util

---
+++ client/utils/http.ts
@@ -0,0 +1,29 @@
+// http request util for client
+import axios from 'axios'
+
+const defaultHeaders = {
+  'Accept': 'application/json, text/plain, */*; charset=utf-8',
+  'Content-Type': 'application/json; charset=utf-8',
+  'Pragma': 'no-cache',
+  'Cache-Control': 'no-cache',
+}
+
+const axiosInstance = axios.create({ headers: defaultHeaders })
+
+axiosInstance.defaults.withCredentials = true
+
+axiosInstance.interceptors.request.use(
+  // 拦截请求，添加 api 前缀
+  (config) => {
+    config.url = `/api${config.url}`
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+axiosInstance.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error)
+)
+
+export default axiosInstance
