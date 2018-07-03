a4843a2fa6931159406f09be0238bfaab6d91c83
dogrod <rodrickjue@hotmail.com>
2018-07-03 18:24:56 +0800

    :sparkles: API urls

---
+++ client/api/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @file Universal API config
+ */
+export default {
+  getPostList: '/v1/blog/posts',
+  getPostDetail: '/v1/blog/posts/', // /:postSlug
+  getPostListWithTag: '/v1/blog/posts/tag' // /:tagSlug
+}
