0a656003c30a9facc4599062c9c1d0c6c1a0da29
dogrod <rodrickjue@hotmail.com>
2018-07-09 18:09:12 +0800

    :sparkles: Fetch post detail data

--- client/pages/post-detail/index.tsx
+++ client/pages/post-detail/index.tsx
@@ -1,7 +1,63 @@
 import * as React from 'react'
+import { RouteComponentProps } from 'react-router'
 
-class PostDetail extends React.Component {
-  public render() {
+import BlogTypes from '@/types/blog'
+
+import http from '@/utils/http'
+import api from '@/api'
+
+interface PropTypes extends RouteComponentProps<{ slug: string }> {}
+
+interface StateTypes {
+  post?: BlogTypes.Post
+  slug: string
+  isLoading: boolean
+}
+
+class PostDetail extends React.Component<PropTypes, StateTypes> {
+  constructor(props: PropTypes) {
+    super(props)
+
+    this.state = {
+      slug: props.match.params.slug,
+      isLoading: false
+    }
+  }
+
+  async componentDidMount() {
+    this.setLoadingStatus(true)
+
+    try {
+      const post = await this.fetchPostDetailData()
+
+      this.setState({
+        post
+      })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      this.setLoadingStatus(false)
+    }
+  }
+
+  async fetchPostDetailData() {
+    const { slug } = this.state
+    const url = `${api.getPosts}/${slug}`
+    
+    try {
+      const response: any = await http.get(url)
+
+      return response.result
+    } catch (error) {
+      throw error
+    }
+  }
+
+  setLoadingStatus(isLoading: boolean) {
+    this.setState({ isLoading })
+  }
+
+  render() {
     return (
       <div>
         Post Detail
