59a22bfb078a62f144272d9d4ada3c4a5c1d7621
dogrod <rodrickjue@hotmail.com>
2018-07-23 13:52:24 +0800

    :sparkles: Login page

---
+++ client/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        Login
+      </div>
+    )
+  }
+}
+
+export default Login
--- client/routes.tsx
+++ client/routes.tsx
@@ -3,6 +3,7 @@ import { Route, Redirect } from 'react-router-dom'
 
 import PostList from './pages/post-list'
 import PostDetail from './pages/post-detail'
+import Login from './pages/login'
 import Tag from './pages/tag'
 
 export default [
@@ -10,5 +11,6 @@ export default [
   <Route key="post-detail" path="/post/:slug" component={PostDetail} />,
   <Route key="tag" path="/tags/:tagName" component={PostList} />,
   <Route key="tags" path="/tags" component={Tag} />,
+  <Route key="login" path="/login" component={Login} />,
   <Redirect key="not-found" to="/" />
 ]
