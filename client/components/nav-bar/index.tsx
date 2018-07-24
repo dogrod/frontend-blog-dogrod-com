48ab853e30ad6c8b2f66b2b17b0745efe9db5a5f
dogrod <rodrickjue@hotmail.com>
2018-07-24 14:36:28 +0800

    :lipstick: NavBar stylesheet

--- client/components/nav-bar/index.tsx
+++ client/components/nav-bar/index.tsx
@@ -1,15 +1,58 @@
 import * as React from 'react'
+import classNames from 'classnames'
 import { Link } from 'react-router-dom'
 
 import logo from '@/assets/images/icon_d.svg'
 import './index.scss'
 
+interface StateProps {
+  isLoggedIn: boolean
+  profile: {
+    username: string
+    email: string
+  }
+}
+
 const PREFIX_CLASS = 'header'
 
-class NavBar extends React.Component {
+class NavBar extends React.Component<{}, StateProps> {
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      isLoggedIn: false,
+      profile: {
+        username: '',
+        email: '',
+      }
+    }
+  }
+
+  renderProfileOrLogin() {
+    const { isLoggedIn, profile } = this.state
+
+    const className = classNames({
+      [`${PREFIX_CLASS}__user`]: true,
+      [`${PREFIX_CLASS}__logged`]: isLoggedIn,
+      [`${PREFIX_CLASS}__unlogged`]: !isLoggedIn,
+    })
+
+    return isLoggedIn ? (
+      <div className={className}>
+        <a>{profile.username}</a>
+      </div>
+    ) : (
+      <div className={className}>
+        <Link to="/login">登录</Link>
+        /
+        <Link to="/login">注册</Link>
+      </div>
+    )
+  }
+
   renderTopBar() {
     return (
-      <div className={`${PREFIX_CLASS}__top-bar`}>
+      <div className={`${PREFIX_CLASS}__meta-bar`}>
         <Link to="/" className={`${PREFIX_CLASS}__logo`}>
           <img src={logo} />
         </Link>
@@ -31,6 +74,7 @@ class NavBar extends React.Component {
             <a href="//www.github.com/dogrod" target="_blank">GIT HUB</a>
           </li>
         </ul>
+        {this.renderProfileOrLogin()}
       </div>
     )
   }
