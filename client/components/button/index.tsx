4efb53004776fe5bacb2646400b1c83c08328a98
dogrod <rodrickjue@hotmail.com>
2018-07-27 18:27:37 +0800

    :lipstick: Button component

--- client/components/button/index.tsx
+++ client/components/button/index.tsx
@@ -1,55 +1,5 @@
-import * as React from 'react'
-import classNames from 'classnames'
-import omit from 'omit.js'
+import Button from './button'
 
-interface PropState extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: ButtonSize
-  theme?: ButtonTheme
-}
-
-export enum ButtonTheme {
-  DEFAULT = 'DEFAULT',
-  PRIMARY = 'PRIMARY',
-  SECONDARY = 'SECONDARY',
-}
-
-export enum ButtonSize {
-  DEFAULT = 'DEFAULT',
-  SMALL = 'SMALL',
-  LARGE = 'LARGE',
-}
-
-const PREFIX_CLASS = 'button'
-
-class Button extends React.Component<PropState> {
-  constructor(props: PropState) {
-    super(props)
-  }
-
-  getClassName = () => {
-    const { size, theme } = this.props
-
-    return classNames(
-      PREFIX_CLASS,
-      {
-        [`${PREFIX_CLASS}--small`]: size === ButtonSize.SMALL,
-        [`${PREFIX_CLASS}--large`]: size === ButtonSize.LARGE,
-        [`${PREFIX_CLASS}--primary`]: theme === ButtonTheme.PRIMARY,
-        [`${PREFIX_CLASS}--secondary`]: theme === ButtonTheme.SECONDARY,
-      },
-      this.props.className,
-    )
-  }
-
-  getNativeAttributes = () => omit(this.props, ['size', 'className', 'children'])
-
-  renderButton = () => {
-    return (<button {...this.getNativeAttributes()}>{this.props.children}</button>)
-  }
-
-  render() {
-    return this.renderButton()
-  }
-}
+export { ButtonTheme, ButtonSize } from './button'
 
 export default Button
