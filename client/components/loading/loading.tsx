f2834be314ac96bd41717a81fb7e13a691a4d53d
dogrod <rodrickjue@hotmail.com>
2018-08-15 18:01:18 +0800

    no message

--- client/components/loading/loading.tsx
+++ client/components/loading/loading.tsx
@@ -1,19 +1,35 @@
 import * as React from 'react'
 
+import Card from '@/components/card'
+
+import './loading.scss'
+
 interface PropTypes {
   error?: boolean
+  // Follow document flow
+  follow?: boolean
 }
 
+const PREFIX_CLASS = 'loading'
+
 const Loading: React.SFC<PropTypes> = (props) => {
-  if (props.error) {
-    return (
-      <div>Error!</div>
-    )
-  }
-  return (
-    <div>
-      {props.children}
+  const renderLoadingContent = () => (
+    <div className={PREFIX_CLASS}>
+      <Card>
+        {props.error
+          ? '出错啦 (⇀‸↼‶)'
+          : props.children || '正在努力加载中.. (⃘ ̂͘₎o̮₍ ̂͘ )⃘'
+        }
+      </Card>
     </div>
+  )
+  
+  return (
+    props.follow ? renderLoadingContent() : (
+      <div className={`${PREFIX_CLASS}__wrapper`}>
+        {renderLoadingContent()}
+      </div>
+    )
   )
 }
 
