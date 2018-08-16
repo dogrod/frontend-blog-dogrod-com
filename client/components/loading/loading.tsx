32d69b7029f54772236402e217a8e7420d937753
dogrod <rodrickjue@hotmail.com>
2018-08-16 13:49:04 +0800

    :hammer: Remove follow mode support & optimize style @Loading

--- client/components/loading/loading.tsx
+++ client/components/loading/loading.tsx
@@ -1,41 +1,27 @@
 import * as React from 'react'
-import classNames from 'classnames'
-
-import Card from '@/components/card'
 
 import './loading.scss'
 
 interface PropTypes {
   error?: boolean
-  // Follow document flow
-  follow?: boolean
 }
 
 const PREFIX_CLASS = 'loading'
 
 const Loading: React.SFC<PropTypes> = (props) => {
   const renderLoadingContent = () => (
-    <div
-      className={classNames(
-        PREFIX_CLASS,
-        { [`${PREFIX_CLASS}--follow`]: props.follow }
-      )}
-    >
-      <Card>
-        {props.error
-          ? '出错啦 (⇀‸↼‶)'
-          : props.children || '正在努力加载中.. (⃘ ̂͘₎o̮₍ ̂͘ )⃘'
-        }
-      </Card>
+    <div className={PREFIX_CLASS}>
+      {props.error
+        ? '出错啦 (⇀‸↼‶)'
+        : props.children || '正在努力加载中 ((⃘ ̂͘₎o̮₍ ̂͘ )⃘)'
+      }
     </div>
   )
   
   return (
-    props.follow ? renderLoadingContent() : (
-      <div className={`${PREFIX_CLASS}__wrapper`}>
-        {renderLoadingContent()}
-      </div>
-    )
+    <div className={`${PREFIX_CLASS}__wrapper`}>
+      {renderLoadingContent()}
+    </div>
   )
 }
 
