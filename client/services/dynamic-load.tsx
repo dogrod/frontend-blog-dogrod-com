2cea841b0c9214a905afa917f927e52d901d9ccf
dogrod <rodrickjue@hotmail.com>
2018-08-16 15:10:49 +0800

    :hammer: Private loading component @DynamicLoad

--- client/services/dynamic-load.tsx
+++ client/services/dynamic-load.tsx
@@ -1,11 +1,28 @@
+import React from 'react'
+
 import Loadable from 'react-loadable'
-import Loading from '@/components/loading'
 
 type LoaderType = any
+
+const Loading: React.SFC<Loadable.LoadingComponentProps> = props => {
+  if (props.error) {
+    return <div>Error! <button onClick={props.retry}>Retry</button></div>
+  } else if (props.timedOut) {
+    return <div>Taking a long time... <button onClick={props.retry}>Retry</button></div>
+  } else if (props.pastDelay) {
+    return <div>Loading...</div>
+  } else {
+    return null
+  }
+}
 
 const LoadableComponent = (loader: LoaderType) => Loadable({
   loader,
   loading: Loading,
+  // Delay time before loading.props.pastDelay
+  delay: 500,
+  // Time before passing timeOut to loading component
+  timeout: 2000,
 })
 
 export default LoadableComponent
