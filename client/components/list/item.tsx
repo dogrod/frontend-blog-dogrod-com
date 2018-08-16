3ff4dd089dc8b647fa607491a7622c08a6a564eb
dogrod <rodrickjue@hotmail.com>
2018-08-16 13:48:37 +0800

    :hammer: Support native attributes @ListItem

--- client/components/list/item.tsx
+++ client/components/list/item.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 import './item.scss'
 
+interface PropStyle extends React.LiHTMLAttributes<HTMLLIElement> {}
+
 const PREFIX_CLASS = 'list-item'
 
-const ListItem: React.SFC = (props) => (
-  <li className={PREFIX_CLASS}>{props.children}</li>
-)
+const ListItem: React.SFC<PropStyle> = (props) => {
+  const elementProps = {
+    ...props,
+    className: PREFIX_CLASS,
+  }
+  
+  return (
+    <li {...elementProps}>{props.children}</li>
+  )
+}
 
 export default ListItem
