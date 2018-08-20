5cb9b17b99b07e8375a434b467205036797059e1
dogrod <rodrickjue@hotmail.com>
2018-08-20 10:10:55 +0800

    :hammer: Change loading hint @DynamicLoad

--- client/services/dynamic-load.tsx
+++ client/services/dynamic-load.tsx
@@ -6,11 +6,11 @@ type LoaderType = any
 
 const Loading: React.SFC<Loadable.LoadingComponentProps> = props => {
   if (props.error) {
-    return <div>Error! <button onClick={props.retry}>Retry</button></div>
+    return <div>加载发生错误 (´・Å・`)<button onClick={props.retry}>点击重试</button></div>
   } else if (props.timedOut) {
-    return <div>Taking a long time... <button onClick={props.retry}>Retry</button></div>
+    return <div>加载时间过长 (|||ﾟдﾟ) <button onClick={props.retry}>点击重试</button></div>
   } else if (props.pastDelay) {
-    return <div>Loading...</div>
+    return <div>在正努力加载中 d(`･∀･)b</div>
   } else {
     return null
   }
@@ -22,7 +22,7 @@ const LoadableComponent = (loader: LoaderType) => Loadable({
   // Delay time before loading.props.pastDelay
   delay: 500,
   // Time before passing timeOut to loading component
-  timeout: 2000,
+  timeout: 5000,
 })
 
 export default LoadableComponent
