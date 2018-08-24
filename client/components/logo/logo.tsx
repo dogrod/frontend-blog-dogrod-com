c5e547c1d11b301453830e6b35cbceed12f2a7d4
dogrod <rodrickjue@hotmail.com>
2018-08-24 15:02:48 +0800

    :hammer: Accept className prop @Logo

--- client/components/logo/logo.tsx
+++ client/components/logo/logo.tsx
@@ -7,6 +7,7 @@ import Icon from '@/components/icon'
 import './logo.scss'
 
 interface PropTypes {
+  className?: string
   size?: LogoSize
 }
 
@@ -18,16 +19,17 @@ export enum LogoSize {
 
 const PREFIX_CLASS = 'logo'
 
-const getClassName = (size?: LogoSize) => classNames(
+const getClassName = (props: PropTypes) => classNames(
   PREFIX_CLASS,
   {
-    [`${PREFIX_CLASS}--large`]: size === LogoSize.LARGE,
-    [`${PREFIX_CLASS}--small`]: size === LogoSize.SMALL,
-  }
+    [`${PREFIX_CLASS}--large`]: props.size === LogoSize.LARGE,
+    [`${PREFIX_CLASS}--small`]: props.size === LogoSize.SMALL,
+  },
+  props.className,
 )
 
 const Logo: React.SFC<PropTypes> = (props) => (
-  <Link to="/" className={getClassName(props.size)}>
+  <Link to="/" className={getClassName(props)}>
     <Icon name="d" />
   </Link>
 )
